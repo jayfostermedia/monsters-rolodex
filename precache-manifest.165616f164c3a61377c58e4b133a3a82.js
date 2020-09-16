62f04726823e20be97f55b21563fb4bcc740bbda
Jason Foster <jayfostermedia@gmail.com>
2020-09-15 22:43:13 -0400

    Updates

---
+++ precache-manifest.165616f164c3a61377c58e4b133a3a82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f88a8d5febdf65e577d3c017ed76559a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "305fc21389688b194c7e",
+    "url": "/monsters-rolodex/static/css/main.20dd784e.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "305fc21389688b194c7e",
+    "url": "/monsters-rolodex/static/js/main.2f6e5f89.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
